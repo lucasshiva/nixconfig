9062dcb26d3408e3a07b4337a99814995601232c
Lucas Silva <silva.lucasdev@gmail.com>
2026-01-31 23:40:50 -0300

    feat(git): use delta for diff

--- home.nix
+++ home.nix
@@ -93,12 +93,26 @@
     };
   };
 
+  # Syntax highlighting for git diffs.
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      navigate = true;
+      dark = true;
+      side-by-side = true;
+      line-numbers = true;
+      syntax-theme = "Catppuccin Mocha";
+    };
+  };
+
   programs.git = {
     enable = true;
     settings = {
       user.name = "Lucas Silva";
       user.email = "silva.lucasdev@gmail.com";
       init.defaultBranch = "main";
+      merge.conflictStyle = "zdiff3"; # For `delta`.
     };
   };
 
