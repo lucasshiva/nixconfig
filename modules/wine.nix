6ecfac37b4ca9361dc943e6e256c8848a9e5c924
Lucas Silva <silva.lucasdev@gmail.com>
2026-02-01 21:26:11 -0300

    feat: add calibre-portable via wine

--- modules/wine.nix
+++ modules/wine.nix
@@ -2,6 +2,7 @@
 let
   baseDir = "/mnt/commondata";
   musicBeeDir = "${baseDir}/Apps/MusicBee";
+  calibreDir = "${baseDir}/Apps/Calibre Portable";
   prefixDir = "${config.home.homeDirectory}/wineprefix";
 in
 {
@@ -24,6 +25,25 @@ in
       export WINEPREFIX="$PREFIX"
       wineboot -u
       ln -s ${baseDir} "$PREFIX/dosdevices/f:"
+      winetricks -q allfonts cjkfonts
+      touch "$PREFIX/.hm-installed"
+
+      echo "Calibre setup complete."
+    '')
+
+    # Calibre.
+    # We use the portable version, so there's no need to map any drives.
+    (pkgs.writeShellScriptBin "setup-calibre" ''
+      set -e
+      PREFIX="${prefixDir}/calibre"
+
+      if [ -e "$PREFIX/.hm-installed" ]; then
+        echo "Calibre Wine Prefix already set up."
+        exit 0
+      fi
+
+      export WINEPREFIX="$PREFIX"
+      wineboot -u
       winetricks -q dotnet48 gdiplus allfonts cjkfonts wmp11
       touch "$PREFIX/.hm-installed"
 
@@ -31,18 +51,31 @@ in
     '')
   ];
 
-  xdg.desktopEntries.musicbee = {
-    name = "MusicBee";
-    icon = "${musicBeeDir}/MusicBee.png";
-    comment = "The Ultimate Music Player and Manager";
-    categories = [
-      "AudioVideo"
-      "Audio"
-      "Music"
-    ];
-    exec = ''
-      env WINEPREFIX=${prefixDir}/musicbee wine "${musicBeeDir}/MusicBee.exe"
-    '';
-  };
+  xdg.desktopEntries = {
+    musicbee = {
+      name = "MusicBee";
+      icon = "${musicBeeDir}/MusicBee.png";
+      comment = "The Ultimate Music Player and Manager";
+      categories = [
+        "AudioVideo"
+        "Audio"
+        "Music"
+      ];
+      exec = ''
+        env WINEPREFIX=${prefixDir}/musicbee wine "${musicBeeDir}/MusicBee.exe"
+      '';
+    };
 
+    calibre = {
+      name = "Calibre";
+      icon = "${calibreDir}/calibre.png";
+      comment = "E-book management";
+      categories = [
+        "Viewer"
+      ];
+      exec = ''
+        env WINEPREFIX=${prefixDir}/calibre wine "${calibreDir}/calibre-portable.exe"
+      '';
+    };
+  };
 }
