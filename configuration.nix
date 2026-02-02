a8a31b348fc0da863166c33caadba2c1ff5f3de8
Lucas Silva <silva.lucasdev@gmail.com>
2026-02-02 15:39:04 -0300

    set LC_ALL to `pt_BR`

--- configuration.nix
+++ configuration.nix
@@ -44,17 +44,9 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
+  i18n.extraLocales = [ "pt_BR.UTF-8/UTF-8" ];
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
+    LC_ALL = "pt_BR.UTF-8";
   };
 
   # Enable the X11 windowing system.
