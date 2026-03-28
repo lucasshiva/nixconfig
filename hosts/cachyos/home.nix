6b415d6f5cecfda46afc87b719d209b5e51f7b0f
Lucas Silva <silva.lucasdev@gmail.com>
2026-03-28 19:07:05 -0300

    feat(cachyos): add font config

--- hosts/cachyos/home.nix
+++ hosts/cachyos/home.nix
@@ -11,11 +11,44 @@
   home.packages = with pkgs; [
     nixd
     nil
+
+    # -- Font packages --
+    # The fonts below are usually already installed via other means, usually during installation.
+    # For example, some KDE packages require `noto-fonts` to be installed via pacman.
+    #
+    # noto-fonts
+    # noto-fonts-cjk
+    # noto-fonts-emoji
+    #
+    # Programming fonts
+    nerd-fonts.monaspace
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.dejavu-sans-mono
   ];
 
   my.shell = {
     bash.enable = true;
     fish.enable = true;
+  };
+
+  fonts = {
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = [
+          "MonaspiceNe Nerd Font Mono"
+          "JetBrainsMono Nerd Font Mono"
+          "DejaVuSansM Nerd Font Mono"
+          "DejaVu Sans Mono"
+        ];
+        serif = [ "Noto Serif" ];
+        sansSerif = [ "Noto Sans" ];
+        emoji = [ "Noto Emoji" ];
+      };
+      hinting = "slight";
+      antialiasing = true;
+      subpixelRendering = "rgb";
+    };
   };
 
   home.stateVersion = "25.11";
