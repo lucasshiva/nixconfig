a999ecd6734a6e7eea17b214d5a8cfc187cf4414
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-01 01:47:10 -0300

    feat: make locale settings default for all hosts

--- modules/den/defaults.nix
+++ modules/den/defaults.nix
@@ -9,13 +9,34 @@ in
 
   den.default.nixos = {
     system.stateVersion = stateVersion;
+
+    # Allow unfree packages.
     nixpkgs.config.allowUnfree = true;
+
+    # Enable flakes.
     nix.settings.experimental-features = [
       "nix-command"
       "flakes"
     ];
+
+    # Enable networking.
     networking.networkmanager.enable = true;
 
+    # Locale settings
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+
+    # Configure home-manager.
     home-manager = {
       backupFileExtension = "backup";
       useUserPackages = true;
--- modules/hosts/nyx.nix
+++ modules/hosts/nyx.nix
@@ -36,20 +36,8 @@ in
           options = "compose:rwin";
         };
 
-        # Timezone and locale settings
+        # Timezone. We can hardcode it since this host is a PC.
         time.timeZone = "America/Sao_Paulo";
-        i18n.defaultLocale = "en_US.UTF-8";
-        i18n.extraLocaleSettings = {
-          LC_ADDRESS = "pt_BR.UTF-8";
-          LC_IDENTIFICATION = "pt_BR.UTF-8";
-          LC_MEASUREMENT = "pt_BR.UTF-8";
-          LC_MONETARY = "pt_BR.UTF-8";
-          LC_NAME = "pt_BR.UTF-8";
-          LC_NUMERIC = "pt_BR.UTF-8";
-          LC_PAPER = "pt_BR.UTF-8";
-          LC_TELEPHONE = "pt_BR.UTF-8";
-          LC_TIME = "pt_BR.UTF-8";
-        };
 
         boot.initrd.availableKernelModules = [
           "ahci"
