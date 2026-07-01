f463abfe99fcdfd55634dd3106cdd8c7db501d1a
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-01 17:43:17 -0300

    feat(shell): add zsh

---
+++ modules/features/shell/zsh.nix
@@ -0,0 +1,38 @@
+{ lib, ... }:
+{
+  den.aspects.shell.zsh = {
+    homeManager =
+      { config, pkgs, ... }:
+      {
+        programs.zsh = {
+          enable = true;
+          plugins = [
+            # Other plugins
+          ]
+          ++ lib.optionals config.programs.fzf.enable [
+            {
+              name = "fzf-tab";
+              src = pkgs.zsh-fzf-tab;
+              file = "share/fzf-tab/fzf-tab.plugin.zsh";
+            }
+          ];
+          autosuggestion.enable = true;
+          enableCompletion = true;
+          syntaxHighlighting.enable = true;
+          dotDir = "${config.xdg.configHome}/zsh";
+          history = {
+            size = 500000;
+            ignoreDups = true;
+            ignoreAllDups = true;
+            saveNoDups = true;
+            share = true;
+            extended = true;
+          };
+          completionInit = ''
+            autoload -Uz compinit
+            compinit -u
+          '';
+        };
+      };
+  };
+}
