87b9414152ce4af95835856988f29a76852d27b4
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-06 23:38:41 -0300

    feat(shell): add catppuccin-minimal theme for starship

--- modules/features/shell/starship/starship.nix
+++ modules/features/shell/starship/starship.nix
@@ -1,12 +1,36 @@
 { lib, ... }:
 {
   den.aspects.shell.prompts.starship = {
-    homeManager.programs.starship = {
-      enable = true;
-      enableZshIntegration = true;
-      # Currently using a modified version of the `catppuccin-powerline` preset.
-      # In the future, we could also support different styles.
-      settings = lib.importTOML ./starship.toml;
-    };
+    homeManager =
+      { config, ... }:
+      let
+        cfg = config.my.starship;
+        catppuccin-minimal = lib.importTOML ./catppuccin-minimal.toml;
+        catppuccin-powerline = lib.importTOML ./catppuccin-powerline.toml;
+      in
+      {
+
+        options.my.starship = {
+          theme = lib.mkOption {
+            type = lib.types.enum [
+              "catppuccin-minimal"
+              "catppuccin-powerline"
+            ];
+            default = "catppuccin-minimal";
+            description = "Prompt theme";
+          };
+        };
+
+        config = {
+          programs.starship = {
+            enable = true;
+            enableBashIntegration = lib.mkDefault true;
+            enableZshIntegration = lib.mkDefault true;
+            enableFishIntegration = lib.mkDefault true;
+            enableNushellIntegration = lib.mkDefault true;
+            settings = if cfg.theme == "catppuccin-minimal" then catppuccin-minimal else catppuccin-powerline;
+          };
+        };
+      };
   };
 }
