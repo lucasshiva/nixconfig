522ba1ec271dec0412ad55a2e8e55e83f5e38bd5
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-07 23:54:14 -0300

    feat(fish): disable greeting

--- modules/features/shell/fish.nix
+++ modules/features/shell/fish.nix
@@ -2,6 +2,9 @@
   den.aspects.shell.fish.homeManager = { ... }: {
     programs.fish = {
       enable = true;
+      interactiveShellInit = ''
+        set -g fish_greeting
+      '';
     };
   };
 }
