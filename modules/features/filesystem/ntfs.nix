f3b66c7aa7ae9762c9e54e0ccd19a7e1da411426
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-09 13:59:40 -0300

    void: switch from ntfs to ext4 for shared drive

--- modules/features/filesystem/ntfs.nix
+++ modules/features/filesystem/ntfs.nix
@@ -1,14 +1,25 @@
+# This aspect is a mix of two versions. One uses the new driver (ntfs), the other uses the legacy
+# driver (ntfs-3g). Maybe we could split each version into their own modules, letting us choose
+# between them without commenting/uncommenting a bunch of code.
+#
+# Additional, we could also move the mount of any additional drivers to host config instead of
+# leaving it in hardware config. Then, we could the host to choose a driver so that we could
+# automatically mount filesystems using the chosen one.
+#
+# I won't be doing any of this for now because at the moment I don't really need NTFS anymore.
 { lib, ... }: {
   den.aspects.filesystem.ntfs = {
     nixos = { pkgs, ... }: {
-      # It seems like we don't need to enable the "ntfs" module for the new driver. Needs more testing.
+      # It seems like we don't need to enable the "ntfs" module for the new driver.
+      # Or maybe it is enabled by default.
       # boot.kernelModules = [ "ntfs" ];
 
       # Ensure we're not adding `ntfs-3g` by enabling "ntfs";
-      # boot.supportedFilesystems = lib.mkForce [
-      #   "btrfs"
-      #   "ext4"
-      # ];
+      boot.supportedFilesystems = [
+        "btrfs"
+        "ext4"
+        "ntfs" # This fine because I am now using ext4 for the shared drive.
+      ];
 
       # Disable older ntfs modules
       # boot.blacklistedKernelModules = [
@@ -16,10 +27,11 @@
       #   "ntfs-3g" # slow but super reliable
       # ];
 
-      # Userspace utilities for the new ntfs driver.
+      # Userspace utilities for the new ntfs driver. Not sure if this is useful for the old drivers.
       environment.systemPackages = [ pkgs.ntfsprogs-plus ];
 
-      # Ensures Dolphin (and maybe other file managers) auto mounts NTFS drives with the new in-kernel driver.
+      # Ensures Dolphin (and maybe other file managers) auto mounts NTFS drives with the chosen driver.
+      # I think most file managers use ntfs3, but I had some problems moving big folders with it.
       services.udisks2 = {
         enable = true;
         settings = {
--- modules/hosts/void/hardware.nix
+++ modules/hosts/void/hardware.nix
@@ -45,16 +45,18 @@
 
         # Shared drive for media, code files, games, etc.
         fileSystems."/mnt/data" = {
-          device = "/dev/disk/by-uuid/47BE680932B24776";
+          device = "/dev/disk/by-uuid/a2033fec-beef-410c-bff8-dace057bea19";
           # `ntfs` is the new driver available starting from kernel 7.1
+          #
           # To use it, we have to blacklist ntfs-3g and ntfs3, but neither ntfs3 or the new drive
-          # work well with Steam games, so we're forced to use ntfs-3g.
-          fsType = "ntfs-3g";
+          # work well with Steam games, so we're forced to use ntfs-3g if we want ntfs support.
+          #
+          # I gave up on NTFS for now, so I'll be using ext4 as a shared drive between two linux
+          # systems: NixOS and CachyOS.
+          fsType = "ext4";
           options = [
+            "defaults"
             "nofail"
-            "uid=1000"
-            "gid=1000"
-            "rw"
           ];
         };
 
