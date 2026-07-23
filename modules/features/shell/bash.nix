28d7d7c50f3111246b752e5e0c21e20c654612b8
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-23 16:59:00 -0300

    shell(bash): start fish on interactive sessions
    Useful when it comes to using fish on TTY sessions.

--- modules/features/shell/bash.nix
+++ modules/features/shell/bash.nix
@@ -1,6 +1,14 @@
 { ... }:
 {
   shiv.shell.bash = {
-    homeManager.programs.bash.enable = true;
+    homeManager.programs.bash = {
+      enable = true;
+      initExtra = ''
+        # Start fish for interactive TTY sessions, unless explicitly disabled.
+        if [[ $- == *i* ]] && command -v fish >/dev/null && [[ -z "$BASH_NO_FISH" ]]; then
+          exec fish
+        fi
+      '';
+    };
   };
 }
