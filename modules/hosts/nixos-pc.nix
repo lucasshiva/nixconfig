bcd5f98ad18a63b1d43ebbe2cf84a9a4ca02a693
Lucas Silva <silva.lucasdev@gmail.com>
2026-07-26 23:56:13 -0300

    apps: add fooyin
    I had to add an overlay to update it to the latest version. The one from nixpkgs is super old.

--- modules/hosts/nixos-pc.nix
+++ modules/hosts/nixos-pc.nix
@@ -69,6 +69,7 @@ in
           keepassxc
           obsidian
           jetbrains-toolbox
+          fooyin
         ];
       };
 
--- modules/overlays/nixpkgs-stable.nix
+++ modules/overlays/nixpkgs-stable.nix
@@ -1,15 +1,15 @@
 { inputs, ... }: {
-  shiv.overlays = {
-    nixos =
-      { pkgs, ... }:
-      {
-        nixpkgs.overlays = [
-          (final: prev: {
-            stable = import inputs.nixpkgs-stable {
-              system = pkgs.stdenv.hostPlatform.system;
-            };
-          })
-        ];
-      };
+  shiv.overlays.nixos = { pkgs, ... }: {
+    nixpkgs.overlays = [
+      (final: prev: {
+        stable = import inputs.nixpkgs-stable {
+          system = pkgs.stdenv.hostPlatform.system;
+        };
+      })
+
+      (final: prev: {
+        fooyin = final.callPackage ./_fooyin.nix { };
+      })
+    ];
   };
 }
